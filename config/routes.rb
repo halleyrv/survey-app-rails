995985ffa044fe98709601e9db22c23a9479a09c
Halle <francrv@hotmail.com>
2024-01-05 22:17:00 -0500

    implement authentication with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  root 'pages#landing_page'
+  devise_for :users
+
+  authenticated(:user) do
+    root 'pages#index', as: :authenticated_root
+  end
+
+  unauthenticated(:user) do 
+    root 'pages#landing_page'
+  end
   #root 'index#landing_page'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
